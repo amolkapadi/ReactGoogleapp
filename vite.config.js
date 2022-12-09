1ddc2fec993a499c7f4c641653ab080b6d95c95c
amolkapadi <amolakapadi@gmail.com>
2022-12-09 12:55:50 +0530

    google first

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
+})
+
+// AIzaSyDtoEi0oDSM-T2JPTP8RpZGCB729Rmx7gA
+{/* <script async src="https://cse.google.com/cse.js?cx=07cfa92f6729e4fd3">
+</script>
+<div class="gcse-search"></div> */}
